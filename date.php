64928255602cec347a2a0d17afa5a7b65715e88b
Raphaël <duvalraphael1995@gmail.com>
2018-12-03 10:00:36 +0100

    Statut du calendrier

--- date.php
+++ date.php
@@ -78,86 +78,8 @@ echo "Il y a " . $d2 ." Jour avant le 16 mai 2016 ";
     echo "Dans -22 jours : ".date("Y-m-d h:i:sa", $d) . "<br>";
 ?><br><br>
     <h3>TP les dates</h3>  
-<?php
-$mois_fr = Array("", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août","Septembre", "Octobre", "Novembre", "Décembre");
-if(isset($_GET['mois']) && isset($_GET['annee']))
-{
-    $mois=$_GET['mois'];
-    $annee=$_GET['annee'];
-}
-else
-{
-    $mois=date("n");
-    $annee=date("Y");
-}
-
-$l_day=date("t",mktime(0,0,0,$mois,1,$annee));
-$x=date("N", mktime(0, 0, 0, $mois,1 , $annee));
-$y=date("N", mktime(0, 0, 0, $mois,$l_day , $annee));
-$titre=$mois_fr[$mois]." : ".$annee;
-//echo $l_day;
-?>
-<form name="dt" method="get" action="">
-<select name="mois" id="mois" onChange="change()" class="liste">
-<?php
-    for($i=1;$i<13;$i++)
-    {
-        echo '<option value="'.$i.'"';
-        if($i==$mois)
-            echo ' selected ';
-        echo '>'.$mois_fr[$i].'</option>';
-    }
-?>
-</select>
-<select name="annee" id="annee" onChange="change()" class="liste">
-<?php
-    for($i=2000;$i<2029;$i++)
-    {
-        echo '<option value="'.$i.'"';
-        if($i==$annee)
-            echo ' selected ';
-        echo '>'.$i.'</option>';
-    }
-?>
-</select>
-</form>
-<table class="tableau"><caption><?php echo $titre ;?></caption>
-<tr><th>Lun</th><th>Mar</th><th>Mer</th><th>Jeu</th><th>Ven</th><th>Sam</th><th>Dim</th></tr>
-<tr>
-<?php
-//echo $y;
-$case=0;
-if($x>1)
-    for($i=1;$i<$x;$i++)
-    {
-        echo '<td class="desactive"> </td>';
-        $case++;
-    }
-for($i=1;$i<($l_day+1);$i++)
-{
-    $f=$y=date("N", mktime(0, 0, 0, $mois,$i , $annee));
-    $da=$annee."-".$mois."-".$i;
-    $lien=$lien_redir;
-    $lien.="?dt=".$da;
-    echo "<td";
-    echo" onmouseout='over(this,0,1)'>$i</td>";
-    $case++;
-    if($case%7==0)
-        echo "</tr><tr>";
-    
-}
-if($y!=7)
-    for($i=$y;$i<7;$i++)
-    {
-        echo '<td class="desactive"> </td>';
-    }
-?></tr>
-</table>
-<script type="text/javascript">
-function change()
-{
-    document.dt.submit();
-}
-</script>
+<?php 
+    echo "En cours de progression."
+    ?>
 </body>
 </html>
